dcd4b16038c43be0c997f55b049c78e7cdf65e4a
Paul Beeler <sparksco@gmail.com>
2012-08-19 20:18:42 -0600

    Remove *** extraneous `endif'

--- config/common.mk
+++ config/common.mk
@@ -10,8 +10,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.board=$(TARGET_PRODUCT) \
     ro.goo.rom=sparkscoCM10Nightlies \
     ro.goo.version=$(shell date +%s)
-    
-endif
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
